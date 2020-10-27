0cd76f36ed9b80592ea6d425b24013b1eac9fac9
William <willi4m@zju.edu.cn>
2020-10-27 15:28:30 +0800

    [I] First commit.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+  name='console',
+  packages=['console'],
+  version='1.0.0',
+  description='Utilities for display information in terminal.',
+  author='William Ro',
+  author_email='willi4m@zju.edu.cn',
+  url='https://github.com/WilliamRo/console',
+  download_url='https://github.com/WilliamRo/console/tarball/v1.0.0',
+  keywords=['console', 'terminal'],
+  classifiers=[],
+  license='Apache-2.0',
+)
