ddce396f928aefe61c68d43062a853c7362ec6ec
William <willi4m@zju.edu.cn>
2020-10-29 19:34:54 +0800

    [M][A] added printer and basically reproduced tframe.utils.console

--- console/console.py
+++ console/console.py
@@ -14,13 +14,37 @@
 # ====-==============================================================-==========
 """A class that provides utilities to show information in terminal.
 """
-from sys import stdout
+import logging
+from . import printer
+from functools import wraps
 
 
-DEFAULT_PROMPT = '>>'
+def auto_clear(func):
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    # Get the instance of Console class from args
+    self = args[0]
+    assert isinstance(self, Console)
+    # Clear line if last called function is print_progress
+    # Using 'is' instead of '==' does not work
+    if self._last_func_called == self.print_progress:
+      self.clear_line()
+    self._last_func_called = func
+    # Print
+    return func(*args, **kwargs)
+  return wrapper
 
 
 class Console(object):
+
+  __VERSION__ = (1, 0, 0)
+
+  DEFAULT_TITLE = 'main'
+  DEFAULT_PROMPT = '>>'
+  INFO_PROMPT = '::'
+  SUPPLEMENT_PROMPT = '..'
+  WARNING_PROMPT = '!!'
+  TEXT_WIDTH = 79
 
   def __init__(self, buffer_size=0):
     # Public variables
@@ -28,8 +52,10 @@ class Console(object):
     assert isinstance(buffer_size, int) and buffer_size >= 0
     # Private variables
     self._buffer = []
+    self._title = None
+    self._last_func_called = None
 
-  # region : Properties
+  # region: Properties
 
   @property
   def buffer(self):
@@ -39,9 +65,9 @@ class Console(object):
   def buffer_string(self):
     return '\n'.join(self._buffer)
 
-  # endregion : Properties
+  # endregion: Properties
 
-  # region : Private Methods
+  # region: Private Methods
 
   def _add_to_buffer(self, line):
     assert isinstance(line, str)
@@ -50,25 +76,145 @@ class Console(object):
     self._buffer.append(line)
     if len(self._buffer) > self.buffer_size: self._buffer.pop(0)
 
-  # endregion : Private Methods
+  # endregion: Private Methods
   
-  # region : Public Methods
+  # region: Public Methods
 
-  def show_status(self, status, prompt=None):
-    """Shows status following the specified prompt.
-    :param status: content to be shown.
-    :param prompt: The leading symbol, usually '>>' by default.
+  @auto_clear
+  def write_line(self, text, color=None, highlight=None, attributes=None,
+                 buffer=True, **kwargs):
+    """Write a line with fancy style.
+
+    During parsing, changing the leading character is not supported currently.
+
+    :param text: text to be written
+    :param color: should be in
+          {red, green, yellow, blue, magenta, cyan, white}
+    :param highlight: should be in
+          {on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white}
+    :param attributes: should be a subset of
+          {bold, dark, underline, blink, reverse, concealed}
+    :param buffer: whether to add text to buffer
+    :param kwargs: additional keyword arguments for python print function
+    :return: raw text
     """
-    if prompt is None: prompt = DEFAULT_PROMPT
-    # The type of status should not be restricted
+    text = printer.write_line(text, color, highlight, attributes, **kwargs)
+    # Add to buffer if necessary
+    if buffer: self._add_to_buffer(text)
+    return text
+
+
+  def write(self, text, color=None, highlight=None, attributes=None):
+    """Write the given text with fancy style, as in write_line method.
+
+    During parsing, changing the leading character is not supported currently.
+    This method will neither return anything nor add content into buffer.
+
+    :param text: text to be written
+    """
+    printer.write(text, color, highlight, attributes)
+
+
+  def split(self, splitter='-', text_width=None, color=None, highlight=None,
+            attributes=None):
+    """Split terminal with fancy (if style is specified) splitter.
+
+    Example:
+      console.split('#{-}{red}#{-}{yellow}#{-}{blue}')
+    """
+    text_width = text_width or self.TEXT_WIDTH
+    # Repeat number should be calculated using raw text
+    ren, raw = printer.parse(splitter)
+    num = int(text_width / len(raw))
+    self.write_line(num * ren, color, highlight, attributes)
+
+
+  def start(self, title=None, text_width=None, color='grey'):
+    """Indicate the start of a program."""
+    title = title or self.DEFAULT_TITLE
+    text_width = text_width or self.TEXT_WIDTH
+    self.write_line('-> Start of {}'.format(title), color)
+    self.split(text_width=text_width, color=color)
+    self._title = title
+
+
+  def end(self, text_width=None, color='grey'):
+    """Indicate the end of a program."""
+    text_width = text_width or self.TEXT_WIDTH
+    self.split(text_width=text_width, color=color)
+    self.write_line('|> End of {}'.format(self._title), color)
+
+
+  def section(self, section_title):
+    """Indicate the begin of a section."""
+    self.split()
+    self.write_line(':: {}'.format(section_title))
+    self.split()
+
+
+  @auto_clear
+  def show_status(self, text, color=None, highlight=None, attributes=None,
+                 prompt=None, buffer=True, **kwargs):
+    """Show text following the specified prompt.
+
+    The text will be displayed in a fancy style if corresponding configurations
+    are provided, as in self.write_line method.
+
+    :param text: text to be written.
+    :param prompt: The leading symbol, usually '>>' by default.
+    :param buffer: Whether to add text to buffer
+    """
+    # Use default prompt symbol if not provided
+    if prompt is None: prompt = self.DEFAULT_PROMPT
+    # The type of 'text' should not be restricted
     assert isinstance(prompt, str)
-    line = '{} {}'.format(prompt, status)
-    print(line)
-    self._add_to_buffer(line)
+    prompt_text = '{} {}'.format(prompt, text)
+    # Print using write_line
+    return self.write_line(
+      prompt_text, color, highlight, attributes, buffer=buffer, **kwargs)
 
-  # endregion : Public Methods
 
-  # region : Static Methods
+  def show_info(self, text, color=None, highlight=None, attributes=None):
+    """Show information using self.show_status"""
+    self.show_status(text, color, highlight, attributes, self.INFO_PROMPT)
+
+
+  def supplement(self, text, color=None, highlight=None, attributes=None,
+                 level=1):
+    """Show supplement using self.show_status"""
+    assert isinstance(level, int) and level > 0
+    self.show_status(text, color, highlight, attributes,
+                     self.SUPPLEMENT_PROMPT * level)
+
+
+  def warning(self, text, color='red', highlight=None, attributes=None):
+    """Show supplement using self.show_status"""
+    self.show_status(text, color, highlight, attributes, self.WARNING_PROMPT)
+
+
+  def print_progress(self, index=None, total=None, start_time=None,
+                     progress=None):
+    """Show progress bar using printer.print_progress.
+
+    :param index: positive scalar, indicating current working progress
+    :param total: positive scalar, indicating the scale of total work
+    :param start_time: if provided, ETA will be displayed to the right of
+                        the progress bar
+    :param progress: if provided, 'index' and 'total' will be ignored.
+    """
+    printer.print_progress(index, total, start_time, progress)
+    # This method does not need to be decorated due to the line below
+    self._last_func_called = self.print_progress
+
+
+  @staticmethod
+  def clear_line():
+    """Clear a line in which current cursor is positioned."""
+    printer.clear_line(Console.TEXT_WIDTH)
+
+  # endregion: Public Methods
+
+  # region: Static Methods
 
   @staticmethod
   def disable_logging(pkg_name):
@@ -77,13 +223,9 @@ class Console(object):
                      e.g., 'tensorflow'
     """
     assert isinstance(pkg_name, str)
-    import logging
     logging.getLogger(pkg_name).disabled = True
 
-  # endregion : Static Methods
+  # endregion: Static Methods
 
-
-if __name__ == '__main__':
-  pass
 
 
---
+++ console/printer.py
@@ -0,0 +1,349 @@
+# Copyright 2020 William Ro. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+# This file incorporates work covered by the following copyright and
+# permission notice:
+#
+#   Copyright (c) 2008-2011 Volvox Development Team
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to
+#   deal in the Software without restriction, including without limitation the
+#   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#   sell copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+#   IN THE SOFTWARE.
+#
+#   Author: Konstantin Lepa <konstantin.lepa@gmail.com>
+# ====-==================================================================-======
+"""Methods for printing colored symbols, may be with special attributes.
+
+This module depends on termcolor package.
+See https://pypi.org/project/termcolor/
+
+Examples:
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+from termcolor import colored, cprint
+
+# The lines below will yield exactly the same outputs
+cprint('Hello, World!', 'red', 'on_cyan')
+write_line('Hello, World!', 'red', 'on_cyan')
+write_line('#{Hello, World!}{red}{on_cyan}')
+
+# The lines below will yield exactly the same outputs
+print(colored('Hello', 'red') + ', ' + colored('World', 'green') + '!')
+write_line('#{Hello}{red}, #{World}{green}!')
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+"""
+import os
+import re
+import time
+from sys import stdout
+
+
+__all__ = ['parse', 'write_line', 'write', 'clear_line']
+
+
+# region: Code from termcolor.py
+
+ATTRIBUTES = dict(
+        list(zip([
+            'bold',
+            'dark',
+            '',
+            'underline',
+            'blink',
+            '',
+            'reverse',
+            'concealed'
+            ],
+            list(range(1, 9))
+            ))
+        )
+del ATTRIBUTES['']
+
+
+HIGHLIGHTS = dict(
+        list(zip([
+            'on_grey',
+            'on_red',
+            'on_green',
+            'on_yellow',
+            'on_blue',
+            'on_magenta',
+            'on_cyan',
+            'on_white'
+            ],
+            list(range(40, 48))
+            ))
+        )
+
+
+COLORS = dict(
+        list(zip([
+            'grey',
+            'red',
+            'green',
+            'yellow',
+            'blue',
+            'magenta',
+            'cyan',
+            'white',
+            ],
+            list(range(30, 38))
+            ))
+        )
+
+
+RESET = '\033[0m'
+
+
+def colored(text, color=None, on_color=None, attrs=None):
+    """Colorize text.
+
+    Available text colors:
+        red, green, yellow, blue, magenta, cyan, white.
+
+    Available text highlights:
+        on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white.
+
+    Available attributes:
+        bold, dark, underline, blink, reverse, concealed.
+
+    Example:
+        colored('Hello, World!', 'red', 'on_grey', ['blue', 'blink'])
+        colored('Hello, World!', 'green')
+    """
+    if os.getenv('ANSI_COLORS_DISABLED') is None:
+        fmt_str = '\033[%dm%s'
+        if color is not None:
+            text = fmt_str % (COLORS[color], text)
+
+        if on_color is not None:
+            text = fmt_str % (HIGHLIGHTS[on_color], text)
+
+        if attrs is not None:
+            for attr in attrs:
+                text = fmt_str % (ATTRIBUTES[attr], text)
+
+        text += RESET
+    return text
+
+# endregion: Code from termcolor.py
+
+# region: Enumerates
+
+class TextColors:
+  GREY = 'grey'
+  RED = 'red'
+  GREEN = 'green'
+  YELLOW = 'yellow'
+  BLUE = 'blue'
+  MAGENTA = 'magenta'
+  CYAN = 'cyan'
+  WHITE = 'white'
+
+class TextHighlights:
+  ON_GREY = 'on_grey'
+  ON_RED = 'on_red'
+  ON_GREEN = 'on_green'
+  ON_YELLOW = 'on_yellow'
+  ON_BLUE = 'on_blue'
+  ON_MAGENTA = 'on_magenta'
+  ON_CYAN = 'on_cyan'
+  ON_WHITE = 'on_white'
+
+class Attributes:
+  BOLD = 'bold'
+  DARK = 'dark'
+  UNDERLINE = 'underline'
+  BLINK = 'blink'
+  REVERSE = 'reverse'
+  CONCEALED = 'concealed'
+
+# endregion: Enumerates
+
+# region: Private Methods
+
+def _render(text, *args):
+  """Renders text using colored.
+
+  Arguments in the back, if exist, will overwrite the previous ones,
+  except for ATTRIBUTE arguments, which will be gathered to a list.
+
+  :param text: text to be rendered
+  :param args: arguments for rendering
+  :return: rendered text and raw text
+  """
+  color, highlight, attributes = None, None, []
+  for arg in args:
+    if arg in COLORS.keys():
+      color = arg
+    elif arg in HIGHLIGHTS.keys():
+      highlight = arg
+    elif arg in ATTRIBUTES.keys() and arg not in attributes:
+      attributes.append(arg)
+  return colored(text, color, highlight, attributes), text
+
+# endregion: Private Methods
+
+# region: Public Methods
+
+def parse(text, lead='#'):
+  """Parse the given text according to the syntax.
+
+  Pattern: lead + r'(\{.+?\})+'
+  Here 'lead' is the leading character that can be specified.
+  e.g., if 'lead' is set to '#', the pattern will be r'#(\{.+?\})+'
+
+  Example:
+  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  from termcolor import colored
+
+  rendered, raw = parse('#{Hello}{red}, #{World}{green}!')
+  assert raw == 'Hello, World!'
+  # The lines below will yield exactly the same outputs
+  print(rendered)
+  print(colored('Hello', 'red') + ', ' + colored('World', 'green') + '!')
+  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  :param text: content to be parsed
+  :param lead: the leading character for parsing, '#' by default.
+               BETTER NOT CHANGE !!
+  :return: rendered line for and raw line, see example above
+  """
+  assert isinstance(text, str) and isinstance(lead, str)
+  # Find units to be colored
+  pattern = lead + r'(?:\{.+?\})+'
+  units = re.findall(pattern, text)
+  # Render each unit
+  rendered_text, raw_text = text, text
+  for unit in units:
+    args = re.findall(r'\{(.+?)\}', unit)
+    ren, raw = _render(*args)
+    # Update rendered and raw text
+    rendered_text = rendered_text.replace(unit, ren)
+    raw_text = raw_text.replace(unit, raw)
+
+  return rendered_text, raw_text
+
+
+def write_line(text, color=None, highlight=None, attributes=None, **kwargs):
+  """Write a line with fancy style.
+
+  During parsing, changing the leading character is not supported currently.
+
+  :param text: text to be written
+  :param color: should be in
+        {red, green, yellow, blue, magenta, cyan, white}
+  :param highlight: should be in
+        {on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white}
+  :param attributes: should be a subset of
+        {bold, dark, underline, blink, reverse, concealed}
+  :param kwargs: additional keyword arguments for python print function
+  :return: raw text
+  """
+  ren, raw = parse(text)
+  print(colored(ren, color, highlight, attributes), **kwargs)
+  return raw
+
+
+def write(text, color=None, highlight=None, attributes=None):
+  """Write the given text with fancy style.
+
+  During parsing, changing the leading character is not supported currently.
+
+  :param text: text to be written
+  :param color: should be in
+        {red, green, yellow, blue, magenta, cyan, white}
+  :param highlight: should be in
+        {on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white}
+  :param attributes: should be a subset of
+        {bold, dark, underline, blink, reverse, concealed}
+  """
+  ren, _ = parse(text)
+  stdout.write(colored(ren, color, highlight, attributes))
+  stdout.flush()
+
+
+def print_progress(index=None, total=None, start_time=None, progress=None,
+                   bar_width=65):
+  """Show progress bar.
+
+  This method is inherited from tframe.utils.console.print_progress.
+  The line which the cursor in terminal is positioned will be overwritten.
+
+  :param index: positive scalar, indicating current working progress
+  :param total: positive scalar, indicating the scale of total work
+  :param start_time: if provided, ETA will be displayed to the right of
+                      the progress bar
+  :param progress: if provided, 'index' and 'total' will be ignored.
+  :param bar_width: width of progress bar, 65 by default
+  """
+  # Calculate progress if not provided
+  if progress is None:
+    if index is None or total is None:
+      raise ValueError(
+        'index and total must be given if progress is not provided')
+    progress = 1.0 * index / total
+  progress = min(progress, 1.0)
+
+  # Generate tail
+  if start_time is not None:
+    duration = time.time() - start_time
+    eta = duration / max(progress, 1e-7) * (1 - progress)
+    tail = "ETA: {:.0f}s".format(eta)
+  else:
+    tail = "{:.0f}%".format(100 * progress)
+
+  # Generate progress bar
+  left = int(progress * bar_width)
+  right = bar_width - left
+  mid = '=' if progress == 1 else '>'
+  clear_line()
+  stdout.write('[%s%s%s] %s' %
+               ('=' * left, mid, ' ' * right, tail))
+  stdout.flush()
+
+
+def clear_line(text_width=79):
+  """Clear a line in console."""
+  stdout.write("\r{}\r".format(" " * text_width))
+  stdout.flush()
+
+# endregion: Public Methods
+
+
+if __name__ == '__main__':
+  # The lines below will yield exactly the same outputs
+  print(colored('Hello, World!', 'red', 'on_cyan'))
+  write_line('Hello, World!', 'red', 'on_cyan')
+  write_line('#{Hello, World!}{red}{on_cyan}')
+
+  # The lines below will yield exactly the same outputs
+  print(colored('Hello', 'red') + ', ' + colored('World', 'green') + '!')
+  write_line('#{Hello}{red}, #{World}{green}!')
+  write('#{Hello}{red}, #{World}{green}!')
+
+
+
